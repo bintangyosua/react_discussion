d01633847e5083f87dd9524d7d7356d4942cb7e1
Bintang <animerevindo@gmail.com>
2023-05-19 00:40:26 +0700

    reply

--- src/components/Elements/Navbar/Logo.tsx
+++ src/components/Elements/Navbar/Logo.tsx
@@ -1,5 +1,9 @@
 const Logo = () => {
-  return <a className="btn btn-ghost normal-case text-xl">Discussion</a>;
+  return (
+    <a className="btn btn-ghost normal-case text-xl" href="/">
+      Discussion
+    </a>
+  );
 };
 
 export default Logo;
--- src/components/Elements/Reply/ReplySection.tsx
+++ src/components/Elements/Reply/ReplySection.tsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 interface Reply {
   children: string;
+  reply_id: number;
+  reply_content: string;
+  id_user: string;
+  thread_id: number;
 }
 
 export default function ReplySection() {
+  const [replies, setReplies] = useState<Reply[]>([]);
+  const { thread_id } = useParams();
+
   async function fetchData() {
     try {
-      const result = await fetch("http://localhost:3000/api/threads/");
+      const result = await fetch("http://localhost:3000/api/replies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          thread_id: thread_id,
+        }),
+      });
       if (!result.ok) throw new Error("Request Failed");
       const data = await result.json();
-      setData(data);
+      setReplies(data);
       console.log(data);
     } catch (error) {
       console.error(error);
@@ -21,19 +37,18 @@ export default function ReplySection() {
     fetchData();
   }, []);
 
-  const [data, setData] = useState(0);
-
   return (
-    <div className="flex mb-4">
-      <p className="text-slate-700 text-lg ml-3">Shirayuki</p>
-      <div className="max-w-full max-h-full mx-5 bg-white rounded-xl">
-        <p className="p-4 text-slate-700">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam minus
-          illo placeat commodi ratione dolorum, nisi quidem dolorem qui
-          doloremque reprehenderit fuga corporis architecto laboriosam error
-          voluptatem, consectetur nobis perferendis. Lorem ipsum dolor sit amet
-        </p>
-      </div>
-    </div>
+    <>
+      {replies.map((reply, index) => (
+        <div className="mb-4">
+          <div
+            key={index}
+            className="max-w-full max-h-full mx-5 bg-white rounded-xl">
+            <p className="text-slate-700 text-lg ml-3">{reply.id_user}</p>
+            <p className="p-4 text-slate-700">{reply.reply_content}</p>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
